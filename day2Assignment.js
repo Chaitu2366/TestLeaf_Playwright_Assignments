8ad94b6cf1279acd7d9b9e7da28f8aff1a0a9191
Chaitu2366 <57385545+Chaitu2366@users.noreply.github.com>
2026-06-14 14:04:12 +0530

    Add files via upload
    Day2 assignment

---
+++ day2Assignment.js
@@ -0,0 +1,26 @@
+let userName = "chaiatanya"
+let organization = "Testleaf"
+let contanctNumber = 992667009
+let knowsAutomation = true
+let usesPlaywright = null
+let uses2Playwright
+
+
+console.log(userName);
+console.log(typeof userName);
+
+
+console.log(organization);
+console.log(typeof organization);
+
+console.log(contanctNumber);
+console.log(typeof contanctNumber);
+
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright);
+
+console.log(uses2Playwright);
+console.log(typeof uses2Playwright);
